b9b68be2d1d15bb5e32eeace6ec54db9b7c3f1fd
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-09-09 20:26:26 +0300

    added new files

---
+++ cart/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Purchase
+
+
+class PurchaseForm(forms.ModelForm):
+    class Meta:
+        model = Purchase
+        fields = [
+            'firstname',
+            'lastname',
+            'email',
+            'address',
+            'city',
+            'postcode',
+        ]
