cbd154e1015b8c4a33e97685dfbea4353bdfe870
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-09-09 21:10:09 +0300

    updated

--- shop/urls.py
+++ shop/urls.py
@@ -4,11 +4,11 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('shop/', views.shop, name='shop'),
+    path('shop/', views.sort_items, name='shop'),
     path('item/<int:item_pk>', views.viewitem, name='viewitem'),
     path('item/<int:item_pk>/delete', views.deleteitem, name='delete'),
     path('add_item/', views.additem, name='additem'),
     path('', include('cart.urls')),
+    path('sort_items/', views.sort_items, name='sort_items'),
 ]
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -47,3 +47,20 @@ def deleteitem(request, item_pk):
     if request.method == 'POST':
         item.delete()
         return redirect('shop')
+
+
+def sort_items(request):
+    menu = Menu.objects.all()
+    sort_by = request.GET.get('sort_by')
+    print(f"Sorting by: {sort_by}")
+    items = Item.objects.all()
+    if sort_by == 'price':
+        items = Item.objects.order_by('price')
+    elif sort_by == 'type':
+        items = Item.objects.order_by('type')
+    elif sort_by == 'title':
+        items = Item.objects.order_by('title')
+
+    context = {'items': items, 'menu': menu}
+
+    return render(request, 'shop/shop.html', context)
