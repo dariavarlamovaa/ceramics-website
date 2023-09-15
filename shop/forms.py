8660dcb3bd4cda26ec770636f4b26ddee4910870
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-09-15 21:44:13 +0300

    updated

--- shop/forms.py
+++ shop/forms.py
@@ -7,5 +7,5 @@ class ItemForm(ModelForm):
     class Meta:
         model = Item
         fields = ['title', 'price', 'category', 'image', 'discount_price', 'start_date', 'end_date']
-        widgets = {'start_date': forms.DateTimeInput(attrs={'type': 'date'}),
-                   'end_date': forms.DateTimeInput(attrs={'type': 'date'})}
+        widgets = {'start_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+                   'end_date': forms.DateTimeInput(attrs={'type': 'datetime-local'})}
